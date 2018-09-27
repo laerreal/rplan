17837d319e3d684b9447110c4f157ade68dde1b5
Efimov Vasily <laer.18@gmail.com>
2018-09-28 00:27:14 +0300

    RPlanActivity, common activity class that interoperate with `RPlanService`
    Signed-off-by: Efimov Vasily <laer.18@gmail.com>

---
+++ src/edu/real/android/plan/RPlanActivity.java
@@ -0,0 +1,44 @@
+package edu.real.android.plan;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+
+public class RPlanActivity extends Activity implements ServiceConnection
+{
+
+	protected RPlanService service;
+
+	@Override
+	protected void onStart()
+	{
+		super.onStart();
+
+		bindService(new Intent(getApplicationContext(), RPlanService.class),
+				this, BIND_AUTO_CREATE);
+	}
+
+	@Override
+	protected void onStop()
+	{
+		super.onStop();
+		service = null;
+		unbindService(this);
+	}
+
+	@Override
+	public void onServiceConnected(ComponentName name, IBinder service)
+	{
+		RPlanServiceBinder binder = (RPlanServiceBinder) service;
+		this.service = binder.getService();
+	}
+
+	@Override
+	public void onServiceDisconnected(ComponentName name)
+	{
+		service = null;
+	}
+
+}
