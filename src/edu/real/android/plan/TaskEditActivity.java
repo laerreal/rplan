dfe0392a7f68c57682550f40ec4298105a47b066
Efimov Vasily <laer.18@gmail.com>
2018-09-29 17:53:15 +0300

    TaskEditActivity: note editting
    Signed-off-by: Efimov Vasily <laer.18@gmail.com>

--- src/edu/real/android/plan/TaskEditActivity.java
+++ src/edu/real/android/plan/TaskEditActivity.java
@@ -3,9 +3,13 @@ package edu.real.android.plan;
 import android.content.ComponentName;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import edu.real.external.BiMap;
+import edu.real.plan.Note;
 import edu.real.plan.Task;
+import edu.real.plan.TextNote;
 
 public class TaskEditActivity extends RPlanActivity
 {
@@ -13,6 +17,7 @@ public class TaskEditActivity extends RPlanActivity
 	LinearLayout ll_notes;
 	EditText et_task_description;
 	EditText et_task_name;
+	BiMap<Note, View> note2view;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -23,6 +28,8 @@ public class TaskEditActivity extends RPlanActivity
 		ll_notes = (LinearLayout) findViewById(R.id.ll_task_notes);
 		et_task_description = (EditText) findViewById(R.id.et_task_description);
 		et_task_name = (EditText) findViewById(R.id.et_task_name);
+
+		note2view = new BiMap<Note, View>();
 	}
 
 	@Override
@@ -37,6 +44,18 @@ public class TaskEditActivity extends RPlanActivity
 
 		et_task_name.setText(task.getName());
 		et_task_description.setText(task.getDescription());
+
+		for (Note n : task.getNotes()) {
+			if (n instanceof TextNote) {
+				TextNote tn = (TextNote) n;
+
+				EditText et = new EditText(this);
+				et.setText(tn.getText());
+				ll_notes.addView(et);
+
+				note2view.put(tn, et);
+			}
+		}
 	}
 
 	@Override
@@ -57,6 +76,17 @@ public class TaskEditActivity extends RPlanActivity
 		tmp = et_task_description.getText().toString();
 		if (tmp != task.getDescription()) {
 			task.setDescription(tmp);
+		}
+
+		for (Note n : note2view.keys()) {
+			if (n instanceof TextNote) {
+				EditText et = (EditText) note2view.get(n);
+				TextNote tn = (TextNote) n;
+				tmp = et.getText().toString();
+				if (tmp != tn.getText()) {
+					tn.setText(tmp);
+				}
+			}
 		}
 	}
 }
