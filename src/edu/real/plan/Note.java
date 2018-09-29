d497726736db9cb9d4b8d2412d45b396490153bd
Efimov Vasily <laer.18@gmail.com>
2018-09-29 17:49:48 +0300

    Note: change notification
    Signed-off-by: Efimov Vasily <laer.18@gmail.com>

--- src/edu/real/plan/Note.java
+++ src/edu/real/plan/Note.java
@@ -1,6 +1,25 @@
 package edu.real.plan;
 
+import java.util.Collection;
+import java.util.LinkedList;
+
 public abstract class Note
 {
+	Collection<NoteListener> listeners;
 
+	protected Note()
+	{
+		listeners = new LinkedList<NoteListener>();
+	}
+
+	public void addListener(NoteListener l)
+	{
+		listeners.add(l);
+	}
+
+	protected void notifyChanged() {
+		for (NoteListener l : listeners) {
+			l.onChanged(this);
+		}
+	}
 }
