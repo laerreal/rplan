a33c41550cb5be72e2fbfba34fb05b5cb997fb9f
Efimov Vasily <laer.18@gmail.com>
2018-10-04 01:37:27 +0300

    BiMap: remove redundant `this.`
    Signed-off-by: Efimov Vasily <laer.18@gmail.com>

--- src/edu/real/external/BiMap.java
+++ src/edu/real/external/BiMap.java
@@ -12,35 +12,35 @@ public class BiMap<K, V>
 
 	public BiMap()
 	{
-		this.fwd = new HashMap<K, V>();
-		this.bwd = new HashMap<V, K>();
+		fwd = new HashMap<K, V>();
+		bwd = new HashMap<V, K>();
 	}
 
 	public V put(K key, V value)
 	{
-		this.fwd.put(key, value);
-		this.bwd.put(value, key);
+		fwd.put(key, value);
+		bwd.put(value, key);
 		return value;
 	}
 
 	public K getKey(V value)
 	{
-		return this.bwd.get(value);
+		return bwd.get(value);
 	}
 
 	public V get(K key)
 	{
-		return this.fwd.get(key);
+		return fwd.get(key);
 	}
 
 	public Collection<V> values()
 	{
-		return this.fwd.values();
+		return fwd.values();
 	}
 
 	public Set<K> keys()
 	{
-		return this.fwd.keySet();
+		return fwd.keySet();
 	}
 
 	public V pop(K k)
