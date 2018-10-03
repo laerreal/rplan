9e6671683ac6349547efef69de60634061aef05b
Efimov Vasily <laer.18@gmail.com>
2018-10-03 03:21:19 +0300

    TaskEditActivity: note and subtask addition
    Signed-off-by: Efimov Vasily <laer.18@gmail.com>

--- src/edu/real/android/plan/TaskEditActivity.java
+++ src/edu/real/android/plan/TaskEditActivity.java
@@ -4,6 +4,8 @@ import android.content.ComponentName;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -14,13 +16,16 @@ import edu.real.plan.Subtask;
 import edu.real.plan.Task;
 import edu.real.plan.TextNote;
 
-public class TaskEditActivity extends RPlanActivity
+public class TaskEditActivity extends RPlanActivity implements OnClickListener
 {
 	Task task;
 	LinearLayout ll_notes;
+	int next_note_index;
 	EditText et_task_description;
 	EditText et_task_name;
 	BiMap<Note, View> note2view;
+	Button bt_add_note;
+	Button bt_add_subtask;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -33,6 +38,12 @@ public class TaskEditActivity extends RPlanActivity
 		et_task_name = (EditText) findViewById(R.id.et_task_name);
 
 		note2view = new BiMap<Note, View>();
+		next_note_index = 0;
+
+		bt_add_note = (Button) findViewById(R.id.bt_add_note);
+		bt_add_note.setOnClickListener(this);
+		bt_add_subtask = (Button) findViewById(R.id.bt_add_subtask);
+		bt_add_subtask.setOnClickListener(this);
 	}
 
 	@Override
@@ -54,40 +65,45 @@ public class TaskEditActivity extends RPlanActivity
 		et_task_description.setText(task.getDescription());
 
 		for (Note n : task.getNotes()) {
-			if (n instanceof Subtask) {
-				Subtask st = (Subtask) n;
+			addViewForNote(n);
+		}
+	}
 
-				LinearLayout ll = new LinearLayout(this);
-				ll.setOrientation(LinearLayout.HORIZONTAL);
+	private void addViewForNote(Note n)
+	{
+		if (n instanceof Subtask) {
+			Subtask st = (Subtask) n;
 
-				CheckBox cb = new CheckBox(this);
-				cb.setChecked(st.getChecked());
-				cb.setTag(TaskViewer.TAG_CHECKBOX);
-				ll.addView(cb);
+			LinearLayout ll = new LinearLayout(this);
+			ll.setOrientation(LinearLayout.HORIZONTAL);
 
-				EditText et = new EditText(this);
-				et.setSingleLine(true);
-				et.setText(st.getText());
-				et.setTag(TaskViewer.TAG_NAME);
+			CheckBox cb = new CheckBox(this);
+			cb.setChecked(st.getChecked());
+			cb.setTag(TaskViewer.TAG_CHECKBOX);
+			ll.addView(cb);
 
-				LayoutParams lp = new LayoutParams(LayoutParams.MATCH_PARENT,
-						LayoutParams.WRAP_CONTENT);
+			EditText et = new EditText(this);
+			et.setSingleLine(true);
+			et.setText(st.getText());
+			et.setTag(TaskViewer.TAG_NAME);
 
-				ll.addView(et, lp);
+			LayoutParams lp = new LayoutParams(LayoutParams.MATCH_PARENT,
+					LayoutParams.WRAP_CONTENT);
 
-				ll_notes.addView(ll);
+			ll.addView(et, lp);
 
-				note2view.put(st, ll);
-			} else if (n instanceof TextNote) {
-				TextNote tn = (TextNote) n;
+			ll_notes.addView(ll, next_note_index++);
 
-				EditText et = new EditText(this);
-				et.setSingleLine(true);
-				et.setText(tn.getText());
-				ll_notes.addView(et);
+			note2view.put(st, ll);
+		} else if (n instanceof TextNote) {
+			TextNote tn = (TextNote) n;
 
-				note2view.put(tn, et);
-			}
+			EditText et = new EditText(this);
+			et.setSingleLine(true);
+			et.setText(tn.getText());
+			ll_notes.addView(et, next_note_index++);
+
+			note2view.put(tn, et);
 		}
 	}
 
@@ -139,5 +155,20 @@ public class TaskEditActivity extends RPlanActivity
 				}
 			}
 		}
+	}
+
+	@Override
+	public void onClick(View v)
+	{
+		Note n;
+		if (v == bt_add_note) {
+			n = new TextNote();
+		} else if (v == bt_add_subtask) {
+			n = new Subtask();
+		} else {
+			return;
+		}
+		task.addNote(n);
+		addViewForNote(n);
 	}
 }
