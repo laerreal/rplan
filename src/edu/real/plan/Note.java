96ccbf5dcd3eb75174ff03c59e1561019fe1bba2
Efimov Vasily <laer.18@gmail.com>
2018-10-22 01:52:22 +0300

    Note: loading
    Signed-off-by: Efimov Vasily <laer.18@gmail.com>

--- src/edu/real/plan/Note.java
+++ src/edu/real/plan/Note.java
@@ -12,4 +12,19 @@ public abstract class Note extends Notifier<NoteListener>
 	}
 
 	public abstract void save(StringWriter w);
+
+	public static final Note load(String line)
+	{
+		if (line.startsWith(TextNote.PREFIX)) {
+			return new TextNote(line.substring(TextNote.PREFIX.length()));
+		} else if (line.startsWith(Subtask.CHECKED_PREFIX)) {
+			return new Subtask(line.substring(Subtask.CHECKED_PREFIX.length()),
+					true);
+		} else if (line.startsWith(Subtask.UNCHECKED_PREFIX)) {
+			return new Subtask(
+					line.substring(Subtask.UNCHECKED_PREFIX.length()),
+					false);
+		}
+		return null;
+	}
 }
