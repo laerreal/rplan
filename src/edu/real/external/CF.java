22e34aaf95d5acb928813ccac724a25aeb283114
Vasily E <laer.18@gmail.com>
2021-01-02 20:50:37 +0300

    CF: auto bit assignments
    Signed-off-by: Vasily E <laer.18@gmail.com>

--- src/edu/real/external/CF.java
+++ src/edu/real/external/CF.java
@@ -6,9 +6,11 @@ public class CF
 
 	/* Less value - more prints */
 	static public final int DEBUG = 0;
-	static public final int DEBUG_LOG_TASK_VIEW_INVALIDATES = 1;
-	static public final int DEBUG_LOG_TASK_VIEW_LAYOUT_PARAMS = 2;
-	static public final int DEBUG_NOTIFIER = 4;
+	static private int bit = 0;
+
+	static public final int DEBUG_LOG_TASK_VIEW_INVALIDATES = 1 << (bit++);
+	static public final int DEBUG_LOG_TASK_VIEW_LAYOUT_PARAMS = 1 << (bit++);
+	static public final int DEBUG_NOTIFIER = 1 << (bit++);
 
 	static public final int DEBUG_FLAGS = 0
 		// | DEBUG_LOG_TASK_VIEW_INVALIDATES
